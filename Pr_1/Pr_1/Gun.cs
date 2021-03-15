c25e35c7a70a4df2920ebe55287706931fae2b64
Slamaster-2002 <diamondslav@gmail.com>
2021-03-15 15:53:34 +0300

    Gun без конструктора

---
+++ Pr_1/Pr_1/Gun.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr_1
+{
+    class Gun
+    {
+
+        private string name;
+        private float caliber;
+        private double shotRange;
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public float Caliber
+        {
+            get { return caliber; }
+            set { caliber = value; }
+        }
+        public double ShotRange
+        {
+            get { return shotRange; }
+            set { shotRange = value; }
+        }
+
+        
+    }
+
+
+}
